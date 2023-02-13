6c5fddf978db73b1c1abec5777409a0898464ec3
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-02-13 14:34:37 +0100

    DLL injector

--- code/client/src/main.cpp
+++ code/client/src/main.cpp
@@ -17,3 +17,20 @@ extern "C" void __declspec(dllexport) InitClient(const wchar_t *projectPath) {
     InitFunction::RunAll();
     MH_EnableHook(MH_ALL_HOOKS);
 }
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        Framework::Logging::GetInstance()->SetLogName("HogwartsMP");
+
+        MH_Initialize();
+        hook::set_base();
+
+        // Entry point is handled by an InitFunction, so we just have to enable hooks and trigger the shits down here
+        InitFunction::RunAll();
+        MH_EnableHook(MH_ALL_HOOKS);
+        break;
+    }
+    return 1;
+}
--- code/launcher/src/main.cpp
+++ code/launcher/src/main.cpp
@@ -7,10 +7,30 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     config.destinationDllName = L"HogwartsMPClient.dll";
     config.executableName     = L"HogwartsLegacy.exe";
     config.name               = "HogwartsMP";
+    config.launchType            = Framework::Launcher::ProjectLaunchType::DLL_INJECTION;
     config.platform           = Framework::Launcher::ProjectPlatform::STEAM;
     config.steamAppId         = 990080;
     config.alternativeWorkDir    = L"Phoenix/Binaries/Win64";
     config.useAlternativeWorkDir = true;
+    config.additionalSearchPaths = {
+        L"Engine\\Binaries\\ThirdParty\\DbgHelp",
+        L"Engine\\Binaries\\ThirdParty\\NVIDIA\\GeForceNOW\\Win64",
+        L"Engine\\Binaries\\ThirdParty\\NVIDIA\\NVaftermath\\Win64\\GFSDK_Aftermath_Lib",
+        L"Engine\\Binaries\\ThirdParty\\Ogg\\Win64\\VS2015",
+        L"Engine\\Binaries\\ThirdParty\\Steamworks\\Steamv154\\Win64",
+        L"Engine\\Binaries\\ThirdParty\\Windows\\XAudio2_9\\x64",
+        L"Engine\\Plugins\\Runtime\\Intel\\XeSS\\Binaries\\ThirdParty\\Win64",
+        L"Engine\\Plugins\\Runtime\\Nvidia\\Ansel\\Binaries\\ThirdParty",
+        L"Engine\\Plugins\\Runtime\\Nvidia\\DLSS\\Binaries\\ThirdParty\\Win64",
+        L"Engine\\Plugins\\Runtime\\Nvidia\\NVIDIAGfeSDK\\ThirdParty\\NVIDIAGfeSDK\\redist\\Win64",
+        L"Engine\\Plugins\\Runtime\\Nvidia\\Streamline\\Binaries\\ThirdParty\\Win64",
+        L"Engine\\Plugins\\Runtime\\Nvidia\\Streamline\\Binaries\\ThirdParty\\Win64\\sl",
+        L"Phoenix\\Binaries\\Win64\\EOSSDK-Win64",
+        L"Phoenix\\Binaries\\Win64",
+        L"Phoenix\\Plugins\\ChromaSDKPlugin\\Binaries\\Win64",
+        L"Phoenix\\Plugins\\Wwise\\ThirdParty\\x64_vc160\\Release\\bin",
+        L"Phoenix\Plugins\Wwise\ThirdParty\x64_vc160\Release\bin",
+    };
 
 #ifdef FW_DEBUG
     config.allocateDeveloperConsole = true;
