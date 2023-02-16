e34a5131a1c56f2a5b439ec72183f5dfc92f7275
Segfault <5221072+Segfaultd@users.noreply.github.com>
2023-02-16 16:09:22 +0100

    Render device hooking

--- code/client/src/core/hooks/render_device.cpp
+++ code/client/src/core/hooks/render_device.cpp
@@ -9,25 +9,44 @@
 
 #include "../application.h"
 
-typedef void(__fastcall *FWindowsWindow__Initialize_t)(void *, void *, float **, HINSTANCE, void *, bool);
+class FD3D12Adapter {
+  public:
+    char pad0[0x18];
+    ID3D12Device *m_pDevice;
+};
+
+class FDWindowsWindow {
+  public:
+    char pad0[0x28];
+    HWND m_pMainWindow;
+};
+
+typedef void(__fastcall *FWindowsWindow__Initialize_t)(FDWindowsWindow *, void *, float **, HINSTANCE, void *, bool);
 FWindowsWindow__Initialize_t FWindowsWindow__Initialize_original = nullptr;
 
 typedef void(__fastcall *FWindowsApplication__ProcessMessage_t)(void *, HWND hwnd, uint32_t msg, WPARAM wParam, LPARAM lParam);
 FWindowsApplication__ProcessMessage_t FWindowsApplication__ProcessMessage_original = nullptr;
 
+typedef void(__fastcall *FD3D12Adapter__CreateRootdevice_t)(FD3D12Adapter *, bool);
+FD3D12Adapter__CreateRootdevice_t FD3D12Adapter__CreateRootdevice_original = nullptr;
+
 void FWindowsApplication__ProcessMessage_Hook(void* pThis, HWND hwnd, uint32_t msg, WPARAM wParam, LPARAM lParam) {
     FWindowsApplication__ProcessMessage_original(pThis, hwnd, msg, wParam, lParam);
 }
 
-void FWindowsWindow__Initialize_Hook(void *pThis, void *app, float **definitions, HINSTANCE inst, void *parent, bool showNow) {
+void FWindowsWindow__Initialize_Hook(FDWindowsWindow *pThis, void *app, float **definitions, HINSTANCE inst, void *parent, bool showNow) {
     FWindowsWindow__Initialize_original(pThis, app, definitions, inst, parent, showNow);
 
     // Acquire the windows and patch the title
-    const HWND hWnd = *(HWND*)((DWORD*)pThis+0x28);
-    HogwartsMP::Core::gGlobals.window = hWnd;
-    SetWindowTextA(hWnd, "Hogwarts: Advanced Multiplayer Edition");
+    HogwartsMP::Core::gGlobals.window = pThis->m_pMainWindow;
+    SetWindowTextA(pThis->m_pMainWindow, "Hogwarts: Advanced Multiplayer Edition");
 
-    Framework::Logging::GetLogger("Hooks")->info("Main Window created at {} (show now {})", fmt::ptr(hWnd), showNow ? "yes" : "no");
+    Framework::Logging::GetLogger("Hooks")->info("Main Window created (show now {}) = {}", showNow ? "yes" : "no", fmt::ptr(pThis->m_pMainWindow));
+}
+
+void FD3D12Adapter__CreateRootdevice_Hook(FD3D12Adapter *pThis, bool withDebug) {
+    FD3D12Adapter__CreateRootdevice_original(pThis, withDebug);
+    Framework::Logging::GetLogger("Hooks")->info("D3D12 RootDevice created (with debug {}) = {}", withDebug ? "yes" : "no", fmt::ptr(pThis->m_pDevice));
 }
 
 static InitFunction init([]() {
@@ -38,4 +57,7 @@ static InitFunction init([]() {
     // Initialize our FWindowsApplication ProcessMessage method
     const auto FWindowsApplication__ProcessMessage_Addr = hook::get_opcode_address("E8 ? ? ? ? 48 8B 5C 24 ? 48 8B 6C 24 ? 48 8B 74 24 ? 48 98");
     MH_CreateHook((LPVOID)FWindowsApplication__ProcessMessage_Addr, (PBYTE)FWindowsApplication__ProcessMessage_Hook, reinterpret_cast<void **>(&FWindowsApplication__ProcessMessage_original));
+
+    const auto FD3D12Adapter__CreateRootDevice_Addr = hook::pattern("48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 44 0F B6 FA").get_first();
+    MH_CreateHook((LPVOID)FD3D12Adapter__CreateRootDevice_Addr, (PBYTE)FD3D12Adapter__CreateRootdevice_Hook, reinterpret_cast<void **>(&FD3D12Adapter__CreateRootdevice_original));
 });
