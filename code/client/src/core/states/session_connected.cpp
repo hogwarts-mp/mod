6273558a0a58189ffe4afec3242456ea93af967b
Dominik Madarász <zaklaus@outlook.com>
2023-02-18 13:50:43 +0100

    fix typo in state UI text

--- code/client/src/core/states/session_connected.cpp
+++ code/client/src/core/states/session_connected.cpp
@@ -42,7 +42,7 @@ namespace HogwartsMP::Core::States {
             }
 
             DrawCornerText(CORNER_RIGHT_TOP, "YOU ARE CONNECTED");
-            DrawCornerText(CORNER_RIGHT_TOP, "Press F9 to return to disconnect");
+            DrawCornerText(CORNER_RIGHT_TOP, "Press F9 to disconnect");
         });
 
         if (gApplication->GetInput()->IsKeyPressed(FW_KEY_F9)) {
